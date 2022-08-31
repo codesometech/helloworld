618799e48d2bc056062e735a77646e7e54b5dd81
Praveen Nerellapalli <pravnere@amazon.com>
2022-08-31 15:53:26 -0400

    helm chart

--- src/main/java/com/demo/helloworld/HelloWorldController.java
+++ src/main/java/com/demo/helloworld/HelloWorldController.java
@@ -1,11 +1,17 @@
 package com.demo.helloworld;
 
 import java.net.InetAddress;
+
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.UnknownHostException;
 import java.util.Date;
+import java.util.Map;
 
 @RestController
 public class HelloWorldController {
@@ -23,6 +29,19 @@ public class HelloWorldController {
             ipaddress = "anonymous";
         }
         return "I am " + ipaddress + ". Current time here is " + new Date(System.currentTimeMillis()).toString();
+    }
+
+    @GetMapping("/listHeaders")
+    public ResponseEntity<String> listAllHeaders(
+            @RequestHeader Map<String, String> headers) {
+        StringBuffer header = new StringBuffer();
+        headers.forEach((key, value) -> {
+            header.append(String.format("Header '%s' = %s", key, value));
+            header.append("<br>");
+            header.append(String.format("Header '%s' = %s", key, value));
+        });
+
+        return new ResponseEntity<String>(header.toString(), HttpStatus.OK);
     }
 
     @GetMapping("/passcode")
